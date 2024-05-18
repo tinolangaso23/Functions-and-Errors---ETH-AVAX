b50bfd56902622c3359d750e29266ec445829c81
Regie Prieto <131588448+tinolangaso23@users.noreply.github.com>
2024-05-18 22:32:56 +0800

    Update Prieto.sol

--- Prieto.sol
+++ Prieto.sol
@@ -1,14 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract PersonAge {
-    
-    function verifyAge(uint age) public pure returns (bool) {
-        
-        require(age >= 18, "You must be 18 or older to access this function.");
-        
-        assert(age >= 18);
-        
-        return true;
+contract BasicToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _initialSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply * 10**uint256(_decimals);
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0), "Invalid recipient address");
+    require(_value > 0, "Value must be greater than zero");
+    require(_value <= balances[msg.sender], "Insufficient balance");
+    require(_value <= 100, "Transfer amount exceeds the limit"); 
+
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+}
+
+
+
+    function assertTest() public pure {
+        assert(1 == 2);
+    }
+
+    function revertTest() public pure {
+        revert("This is a revert test");
+    }
+
+    function requireTest(uint256 _value) public pure {
+        require(_value > 0, "Value must be greater than zero");
     }
 }
